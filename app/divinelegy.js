6a1729f3631945a8ee5c4a52358194ab8686ef15
Cameron Ball <c.ball1729@gmail.com>
2016-03-02 01:49:23 +0800

    Limit contributers display. Fixes #13

--- app/divinelegy.js
+++ app/divinelegy.js
@@ -3,8 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('DivinElegy', [
   'ngRoute',
-  'ngAnimate',
-  //'DivinElegy.components.hello',
+  'alv-ch-ng.text-truncate',
   'DivinElegy.components.menu',
   'DivinElegy.components.userMenu',
   'DivinElegy.components.simfiles',
